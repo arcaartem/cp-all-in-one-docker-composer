3b40c2a5ab03d92b9691496941f47199a2accac6
Arca <arca.artem@gmail.com>
2023-05-21 11:45:22 +0100

    Rename generated docker compose file to be the default name

--- run.rb
+++ run.rb
@@ -2,4 +2,4 @@
 require_relative './lib/confluent_platform_composer.rb'
 
 builder = ConfluentPlatformComposer.new(zookeeper_ensemble_size: 3, broker_cluster_size: 3)
-File.write('docker-compose-generated.yml', builder.compose)
+File.write('docker-compose.yml', builder.compose)
